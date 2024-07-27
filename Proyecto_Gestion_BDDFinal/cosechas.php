d7425ffd2bda9c6576a6803d2090c137fc703c1e
JhonGta <jhonguaman-10@hotmail.com>
2024-07-27 14:42:42 -0500

    commit prueba
    ñññ

--- Proyecto_Gestion_BDDFinal/cosechas.php
+++ Proyecto_Gestion_BDDFinal/cosechas.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Cosechas</title>
+    <title>ñññ de Cosechas</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
